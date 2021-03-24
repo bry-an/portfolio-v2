d2d962e32c8b0acab1c7da1c79849af65e61c910
Bryan Yunis <bryany@booj.com>
2021-03-24 03:29:54 -0400

    Revert "include terminal in small screen size"
    This reverts commit 0800ec69472ca10781f5686083bbe5ffbfc9b8eb.
    
    Unfortunately, terminal does not work with mobile devices :(

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex md:flex-row flex-col intro">
-    <div class="md:w-1/2 w-full md:text-right text-center">
+  <div class="flex intro">
+    <div class="sm:w-1/2 w-full sm:text-right text-center">
       <p>
         I am a software engineer who writes a lot of <span class="text-andes">JavaScript</span>
       </p>
@@ -8,11 +8,11 @@
         I build frontend applications with <span class="text-andes">Vue.js</span>
         <span class="block my-3"> and APIs with <span class="text-andes">Node.js</span></span>
       </p>
-      <p class="md:text-right mt-12">
+      <p class="sm:text-right mt-12">
         But on the right team, we can build <span class="text-andes">anything</span>
       </p>
     </div>
-    <div class="md:w-1/2 flex flex-col items-center justify-center mt-3 md:mt-0 md:ml-6">
+    <div class="w-1/2 hidden sm:flex flex-col items-center justify-center ml-6">
       <div class="w-4/5">
         <Terminal />
       </div>
