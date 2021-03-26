fd40859f736a0d195cc71e5c89761e599ed0efba
Bryan Yunis <bryany@booj.com>
2021-03-26 01:30:46 -0400

    change nav item colors to correspond with page location

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="header-container">
+  <div
+    id="kitten"
+    class="header-container"
+  >
     <div class="header flex flex-col xl:flex-row justify-center xl:justify-between items-center xl:items-baseline">
       <div class="flex flex-col sm:flex-row items-center sm:items-baseline">
         <h1 class="tracking-wide inline header-name">
@@ -11,25 +14,25 @@
       </div>
       <div class="flex justify-center items-baseline nav-box">
         <span
-          class="cursor-pointer mr-6 xl:mr-12 nav-item"
+          :class="['cursor-pointer mr-6 xl:mr-12 nav-item', {'text-andes': scrollPos < navItemPositions.vision }]"
           @click="scroll('home')"
         >
           Home
         </span>
         <span
-          class="cursor-pointer mr-6 xl:mr-12 nav-item"
+          :class="['cursor-pointer mr-6 xl:mr-12 nav-item', {'text-andes': scrollPos > navItemPositions.vision && scrollPos < navItemPositions.projects }]"
           @click="scroll('vision')"
         >
           Vision
         </span>
         <span
-          class="cursor-pointer nav-item mr-6 xl:mr-12"
+          :class="['cursor-pointer mr-6 xl:mr-12 nav-item', {'text-andes': scrollPos > navItemPositions.projects && scrollPos < navItemPositions.contact }]"
           @click="scroll('projects')"
         >
           Projects
         </span>
         <span
-          class="cursor-pointer nav-item"
+          :class="['cursor-pointer nav-item', {'text-andes': scrollPos === navItemPositions.contact }]"
           @click="scroll('contact')"
         >
           Contact
@@ -41,13 +44,24 @@
 </template>
 
 <script>
-import { scroll } from '../utils/sharedMethods.js'
+import { onMounted, ref } from 'vue'
+import { scroll, scrollListener, getNavItemPositions, navItemPositions } from '../utils/sharedMethods.js'
 
 export default {
   name: 'Header',
   setup() {
+    const scrollPos = ref(0)
+    onMounted(() => {
+      // issue with Vue 3 comp api, DOM not yet fully rendered onMounted, thus setTimeout :(
+      setTimeout(() => {
+        getNavItemPositions()
+      })
+      scrollListener(scrollPos)
+    })
     return {
       scroll,
+      scrollPos,
+      navItemPositions,
     }
   },
 }
@@ -64,6 +78,7 @@ export default {
     width: 90%;
     margin: auto;
     .header-name {
+      color: $bry-andes;
       line-height: 9.5vmin;
       font-size: $font-large;
       @include md {
@@ -87,8 +102,8 @@ export default {
       }
       font-size: $font-small;
       .nav-item {
-        &:hover {
-          font-weight: 700;
+        &.text-andes {
+          color: $bry-andes;
         }
       }
     }
--- src/utils/sharedMethods.js
+++ src/utils/sharedMethods.js
@@ -1,6 +1,23 @@
-export function scroll(id) {
+import { reactive } from 'vue'
+
+const navItemPositions = reactive({ vision: null, projects: null, contact: null })
+
+function getNavItemPositions() {
+  navItemPositions.vision = document.getElementById('vision').getBoundingClientRect().top + window.pageYOffset - 90
+  navItemPositions.projects = document.getElementById('projects').getBoundingClientRect().top + window.pageYOffset - 90
+  navItemPositions.contact = document.body.offsetHeight - window.innerHeight
+}
+
+function scroll(id) {
   const yOffset = -90
   const element = document.getElementById(id)
-  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset
+  const y = Math.ceil(element.getBoundingClientRect().top + window.pageYOffset + yOffset)
   window.scrollTo({ top: id === 'home' ? 0 : y, behavior: 'smooth' })
 }
+
+function scrollListener(ref) {
+  document.addEventListener('scroll', () => {
+    ref.value = window.scrollY
+  })
+}
+export { scrollListener, scroll, getNavItemPositions, navItemPositions }
